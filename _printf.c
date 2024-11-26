91207ed154ff65620fa26de0505f9d21a4c0ff64
lucas2mz <lucasandrada2606@gmail.com>
2024-11-26 18:22:17 +0000

    Primer commit

--- _printf.c
+++ _printf.c
@@ -4,30 +4,33 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i, j;
-  char c;
-  char (*flags)(char *);
-  char *str;
-  
-  va_start(args, format);
+	va_list args;
+	int i, j;
+	char c;
+	char (*flags)(char *);
+	char *str;
 
-  for (i = 0; format[i] != '\0'; i++)
-    {
-      if (format[i] == '"')
-          {
-            while (format[i] != '"' && format[i] != '%' && format[i] != '\0')
-              {
-                write(1, &format[i], 1);
-                i++;
-              }
-            else if (format[i] == '%')
-          {
-            get_functions(format[i + 1]);
-            if (get_functions != 0)
-              
-          }
-          }
-    }
-  
+	va_start(args, format);
+
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '"')
+          	{
+			while (format[i] != '"' && format[i] != '%' && format[i] != '\0')
+			{
+				write(1, &format[i], 1);
+				i++;
+			}
+		}
+		else if (format[i] == '%')
+		{
+			get_functions(format[i + 1]);
+				if (!get_functions)
+				{
+					return;
+				}
+		}
+	}
+
 }
